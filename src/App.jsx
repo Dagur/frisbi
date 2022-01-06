2c27c033f112277de427908785f4cc2561aa4af7
Dagur Ammendrup <dagurp@vivaldi.com>
2022-01-06 21:51:57 +0000

    Animations and translations

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import Home from "./Home.jsx";
+import { Routes, Route } from "react-router-dom";
+import { TranslatorProvider } from "react-translate";
+import translations from "./translations.json";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <header className="lang">
+        <a href="/">is</a>
+        <a href="/en">en</a>
+      </header>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <TranslatorProvider translations={translations.is}>
+              <Home />
+            </TranslatorProvider>
+          }
+        />
+        <Route
+          path="/en"
+          element={
+            <TranslatorProvider translations={translations.en}>
+              <Home />
+            </TranslatorProvider>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
---
+++ src/Home.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+import { useTranslate } from "react-translate";
+import "./App.css";
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const discs = [
+  "Putter",
+  "Midrange",
+  "Driver",
+  "Understable",
+  "Overstable",
+  "Wildcard"
+];
+const throws = [
+  "Backhand",
+  "Forehand",
+  "Roller",
+  "Hyzer",
+  "Anhyzer",
+  "Wildcard"
+];
+
+function Home(translations) {
+  const t = useTranslate("App");
+  const [disc, setDisc] = React.useState("");
+  const [yeet, setYeet] = React.useState("");
+  const [counter, setCounter] = React.useState(0);
+
+  useEffect(() => {
+    setDisc(discs[getRandomInt(discs.length)]);
+    setYeet(throws[getRandomInt(throws.length)]);
+  }, []);
+
+  const roll = () => {
+    setDisc(discs[getRandomInt(discs.length)]);
+    setYeet(throws[getRandomInt(throws.length)]);
+    setCounter(counter + 1);
+  };
+
+  const odd = Boolean(counter % 2);
+
+  return (
+    <>
+      <div className="app-main">
+        <p className={`throw ${odd ? "odd" : ""}`}>{t(yeet)}</p>
+        <p className={`throw ${odd ? "" : "odd"}`}>{t(disc)}</p>
+      </div>
+      <button className="throw-button" onClick={roll}>
+        {t("Throw")}
+      </button>
+    </>
+  );
+}
+
+export default Home;
